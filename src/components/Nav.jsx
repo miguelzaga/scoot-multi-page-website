37e0dc0b67974db9e940b60e557d0410a05a2df7
Miguel Ángel Zapata <51008047+miguelzaga@users.noreply.github.com>
2024-11-13 06:08:14 -0500

    nav icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
-
+import logo from "../assets/logo.svg";
+import hamburger from "../assets/icons/hamburger.svg";
 function Nav() {
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link>
-        <ul>
+      <nav className="relative flex h-16 items-center justify-center">
+        <button
+          className="absolute left-8"
+          onClick={() => console.log("open menu")}
+        >
+          <img src={hamburger} alt="open menu" />
+        </button>
+        <Link to="/">
+          <img className="w-[75px]" src={logo} alt="scoot logo" />
+        </Link>
+        <ul className="hidden">
           <li>
             <Link to="/about">About</Link>
           </li>
