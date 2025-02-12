8386caed14366fb3637c6ca47408ed98b98da60c
miguelzaga <miguezaga2@hotmail.com>
2025-02-12 15:55:52 -0500

    btns close mobile nav menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -22,7 +22,8 @@ function Nav() {
             />
           </button>
           <div className="mx-auto flex h-16 max-w-desktop items-center justify-center bg-white md:h-24 md:justify-start md:gap-[58px]">
-            <Link to="/">
+            <Link
+              onClick={() => setIsOpen(false)} to="/">
               <img
                 className="max-w-[75px] md:max-w-[107px]"
                 src={logo}
@@ -34,13 +35,19 @@ function Nav() {
             >
               <ul className="ml-8 mt-16 flex min-w-fit flex-col gap-6 font-mono text-lg/6 font-bold text-white md:m-0 md:flex-row md:items-center md:text-body md:text-dimGrey lg:gap-8">
                 <li>
-                  <Link to="/about">About</Link>
+                  <Link
+                    onClick={() => setIsOpen(false)}
+                    to="/about">About</Link>
                 </li>
                 <li>
-                  <Link to="/locations">Location</Link>
+                  <Link
+                    onClick={() => setIsOpen(false)}
+                    to="/locations">Location</Link>
                 </li>
                 <li>
-                  <Link to="/careers">Careers</Link>
+                  <Link
+                    onClick={() => setIsOpen(false)}
+                    to="/careers">Careers</Link>
                 </li>
               </ul>
               <button className="button fixed bottom-6 left-8 w-48 md:static md:ml-auto md:w-[180px] lg:mr-1">
